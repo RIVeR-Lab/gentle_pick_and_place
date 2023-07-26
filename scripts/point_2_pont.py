ddf6e6f8ee5ce87402e7eb144971558b0c947c43
Collins2 <collinsokara027@yahoo.com>
2023-07-26 10:45:51 -0400

    way point upDATE

--- scripts/point_2_pont.py
+++ scripts/point_2_pont.py
@@ -68,14 +68,24 @@ if __name__ == "__main__":
     for i, val in enumerate([5.0, -1.80, -0.80, -2.0, 1.57, 0.1]):
         home_joint_state[i] = val
 
-    arbitrary_joint_state = kdl.JntArray(6)
+    way_point1 = kdl.JntArray(6)
     for i, val in enumerate([2.2, -2.39, -0.44, -1.902, 1.69, 0.26]):  # also make sure this has the correct number of joints
-        arbitrary_joint_state[i] = val
+        way_point1[i] = val
+
+    way_point2 = kdl.JntArray(6)
+    for i, val in enumerate([3.23, -2.39, -0.46, -1.79, 1.64, 0.26]):  # also make sure this has the correct number of joints
+        way_point2[i] = val
+
+    way_point3 = kdl.JntArray(6)
+    for i, val in enumerate([4.0, -2.41, -0.45, -1.75, 1.64, 0.26]):  # also make sure this has the correct number of joints
+        way_point3[i] = val
 
     robot_arm_motion = RobotArmMotion()
 
     robot_arm_motion.send_arm_traj(home_joint_state)
-    robot_arm_motion.send_arm_traj(arbitrary_joint_state)
+    robot_arm_motion.send_arm_traj(way_point1)
+    robot_arm_motion.send_arm_traj(way_point2)
+    robot_arm_motion.send_arm_traj(way_point3)
     robot_arm_motion.send_arm_traj(home_joint_state)
 
 #     home_joint_state = [5.0, -1.80, -0.80, -2.0, 1.57, 0.1]
