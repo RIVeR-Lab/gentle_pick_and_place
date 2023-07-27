81d44b0b30b7d05f426a43d10652840ae363d081
Collins2 <collinsokara027@yahoo.com>
2023-07-27 16:22:37 -0400

    x,y,z

--- scripts/point_2_pont.py
+++ scripts/point_2_pont.py
@@ -83,8 +83,8 @@ class RobotArmMotion(object):
 
 
 if __name__ == "__main__":
-    home_xyz = [-0.059, -0.37126, -0.15378]  
-    way_point1_xyz = [-0.07645, 0.378, -0.15378]  
+    home_xyz = [0.15, 0.45, 0.3]  
+    way_point1_xyz = [0.15, -0.45, 0.3]  
     # way_point2_xyz = [0.2, 0.3, 0.4]  
     # way_point3_xyz = [0.3, 0.4, 0.5]  
 
