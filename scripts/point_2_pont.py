0cd4b1fe665948925658b41a03413df1777f31b1
Collins2 <collinsokara027@yahoo.com>
2023-08-01 15:51:01 -0400

    update

--- scripts/point_2_pont.py
+++ scripts/point_2_pont.py
@@ -80,66 +80,66 @@ class RobotArmMotion(object):
         q_list = [q[i] for i in range(self.num_joints)]
         return q_list
 
-    # def send_arm_traj(self, q):
-    #     q_list = self.jntarray_to_list(q)
+    def send_arm_traj(self, q):
+        q_list = self.jntarray_to_list(q)
 
-    #     traj_goal = FollowJointTrajectoryGoal()
-    #     traj = JointTrajectory()
-    #     traj.joint_names = self.joint_names
-    #     traj_point = JointTrajectoryPoint()
-    #     traj_point.positions = q_list  # use list version
-    #     traj_point.velocities = [0.0] * self.num_joints
-    #     traj_point.time_from_start = rospy.Time(5.0)
+        traj_goal = FollowJointTrajectoryGoal()
+        traj = JointTrajectory()
+        traj.joint_names = self.joint_names
+        traj_point = JointTrajectoryPoint()
+        traj_point.positions = q_list  # use list version
+        traj_point.velocities = [0.0] * self.num_joints
+        traj_point.time_from_start = rospy.Time(5.0)
 
-    #     traj.points = [traj_point]
-    #     traj_goal.trajectory = traj
+        traj.points = [traj_point]
+        traj_goal.trajectory = traj
 
-    #     self.arm_pos_cli.send_goal(traj_goal)
-    #     self.arm_pos_cli.wait_for_result()
+        self.arm_pos_cli.send_goal(traj_goal)
+        self.arm_pos_cli.wait_for_result()
 
-    def xyz_to_jnt(self, x, y, z):
-        # Define workspace limits
-        min_x, max_x = -0.5, 0.5  # Set your desired limits for X
-        min_y, max_y = -0.45, 0.45  # Set your desired limits for Y
-        min_z, max_z = 0.0, 0.5   # Set your desired limits for Z
+    # def xyz_to_jnt(self, x, y, z):
+    #     # Define workspace limits
+    #     min_x, max_x = -0.5, 0.5  # Set your desired limits for X
+    #     min_y, max_y = -0.45, 0.45  # Set your desired limits for Y
+    #     min_z, max_z = 0.0, 0.5   # Set your desired limits for Z
 
-        # Check if the desired position is within the workspace limits
-        if not (min_x <= x <= max_x and min_y <= y <= max_y and min_z <= z <= max_z):
-            rospy.logwarn("Desired position is out of the robot's workspace.")
-            return None
+    #     # Check if the desired position is within the workspace limits
+    #     if not (min_x <= x <= max_x and min_y <= y <= max_y and min_z <= z <= max_z):
+    #         rospy.logwarn("Desired position is out of the robot's workspace.")
+    #         return None
 
-        desired_pose = kdl.Frame(kdl.Vector(x, y, z))
+    #     desired_pose = kdl.Frame(kdl.Vector(x, y, z))
 
-        # Use IK solver to compute joint angles
-        q_init = kdl.JntArray(self.num_joints)  # Initial guess
-        q_out = kdl.JntArray(self.num_joints)  # Output joint array
-        result = self.pos_ik_solver.CartToJnt(q_init, desired_pose, q_out)
-        if result < 0:
-            rospy.logwarn("IK Solution not found")
-            return None
+    #     # Use IK solver to compute joint angles
+    #     q_init = kdl.JntArray(self.num_joints)  # Initial guess
+    #     q_out = kdl.JntArray(self.num_joints)  # Output joint array
+    #     result = self.pos_ik_solver.CartToJnt(q_init, desired_pose, q_out)
+    #     if result < 0:
+    #         rospy.logwarn("IK Solution not found")
+    #         return None
 
-        return q_out
+    #     return q_out
 
-    def send_arm_traj_xyz(self, x, y, z):
-        q = self.xyz_to_jnt(x, y, z)
-        if q is not None:
-            self.send_arm_traj(q)
+    # def send_arm_traj_xyz(self, x, y, z):
+    #     q = self.xyz_to_jnt(x, y, z)
+    #     if q is not None:
+    #         self.send_arm_traj(q)
 
 
 
 if __name__ == "__main__":
-    home_joint_angles = [5.0, -1.80, -0.80, -2.0, 1.57, 0.1] 
-    way_point1_xyz = [0.2, 0.45, 0.0]  
-    # way_point2_xyz = [0.2, 0.3, 0.4]  
-    # way_point3_xyz = [0.3, 0.4, 0.5]  
+    # home_joint_angles = [5.0, -1.80, -0.80, -2.0, 1.57, 0.1] 
+    # way_point1_xyz = [0.2, 0.45, 0.0]  
+    # # way_point2_xyz = [0.2, 0.3, 0.4]  
+    # # way_point3_xyz = [0.3, 0.4, 0.5]  
 
-    robot_arm_motion = RobotArmMotion()
+    # robot_arm_motion = RobotArmMotion()
 
-    robot_arm_motion.send_arm_traj(home_joint_angles)  
-    robot_arm_motion.send_arm_traj_xyz(*way_point1_xyz)
-    # robot_arm_motion.send_arm_traj_xyz(*way_point2_xyz)
-    # robot_arm_motion.send_arm_traj_xyz(*way_point3_xyz)
-    robot_arm_motion.send_arm_traj(home_joint_angles)  
+    # robot_arm_motion.send_arm_traj(home_joint_angles)  
+    # robot_arm_motion.send_arm_traj_xyz(*way_point1_xyz)
+    # # robot_arm_motion.send_arm_traj_xyz(*way_point2_xyz)
+    # # robot_arm_motion.send_arm_traj_xyz(*way_point3_xyz)
+    # robot_arm_motion.send_arm_traj(home_joint_angles)  
 
 
     # # Defining the Home Position
