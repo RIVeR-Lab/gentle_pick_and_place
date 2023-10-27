b3a8018da0ba57d940d4fb0859d10cd710399637
Collins <collins.okara17@gmail.com>
2023-10-27 13:22:44 -0400

    initial

--- scripts/robot_arm_cutting.py
+++ scripts/robot_arm_cutting.py
@@ -10,7 +10,7 @@ from control_msgs.msg import FollowJointTrajectoryAction, FollowJointTrajectoryG
 import tf2_ros
 import tf2_kdl
 
-class SinusoidalMotion(object):
+class RobotArmMotion(object):
     def __init__(self):
         self.base_link = 'base_link'
         self.ee_link = 'tool0'
@@ -45,7 +45,6 @@ class SinusoidalMotion(object):
         q_list = [q[i] for i in range(self.num_joints)]
         return q_list
 
-
     def send_arm_traj(self, q):
         q_list = self.jntarray_to_list(q)
 
@@ -62,48 +61,19 @@ class SinusoidalMotion(object):
 
         self.arm_pos_cli.send_goal(traj_goal)
         self.arm_pos_cli.wait_for_result()
-        
 
-    def execute_sinusoidal_motion(self, amplitude, frequency):
-        # Starting the sinusoidal motion
-        duration = 2 * np.pi / frequency
-        t = np.arange(0.0, duration, 0.01)
-        positions = amplitude * np.sin(2 * np.pi * frequency * t)
-
-        start_pos = kdl.JntArray(self.num_joints)
-        frame = kdl.Frame()  # Create a new frame object
-        self.pos_fk_solver.JntToCart(start_pos, frame)
-
-        for i, pos in enumerate(positions):
-            desired_pos = frame
-            desired_pos.p[0] += pos  # Adding sinusoidal motion in X axis
-            desired_pos.p[1] += i * 0.01  # Linear motion in Y axis
-
-            q = kdl.JntArray(self.num_joints)
-            self.pos_ik_solver.CartToJnt(self.arm_joints, desired_pos, q)
-            self.send_arm_traj(q)
 
 if __name__ == "__main__":
     home_joint_state = [5.0, -1.80, -0.80, -2.0, 1.57, 0.1]
-    sinusoidal_motion = SinusoidalMotion()
-
-    # User input for amplitude and frequency
-    amplitude = float(input("Enter the amplitude (0.01-0.1): "))
-    frequency = float(input("Enter the frequency (0.0001-0.001 Hz): "))
-
-    # Check if user input is within acceptable range
-    if not (0.01 <= amplitude <= 0.1):
-        print("Invalid amplitude! Please enter a value between 0.01 and 0.1.")
-        exit(1)
-    if not (0.0001 <= frequency <= 0.001):
-        print("Invalid frequency! Please enter a value between 0.0001 and 0.001 Hz.")
-        exit(1)
+    # Define the arbitrary joint angles
+    arbitrary_joint_state = [1.2, -0.8, -1.0, -1.5, 1.5, 0.2]
+    robot_arm_motion = RobotArmMotion()
 
     # Send the robot to home position
-    sinusoidal_motion.send_arm_traj(home_joint_state)
+    robot_arm_motion.send_arm_traj(home_joint_state)
 
-    # Execute sinusoidal motion with user defined amplitude and frequency
-    sinusoidal_motion.execute_sinusoidal_motion(amplitude, frequency)
+    # Send the robot to the arbitrary position
+    robot_arm_motion.send_arm_traj(arbitrary_joint_state)
 
     # Send the robot back to home position
-    sinusoidal_motion.send_arm_traj(home_joint_state)
+    robot_arm_motion.send_arm_traj(home_joint_state)
