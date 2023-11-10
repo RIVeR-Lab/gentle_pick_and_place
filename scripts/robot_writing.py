4eaef6d495358b831b69534eedb581c6e483c838
Collins2 <collinsokara027@yahoo.com>
2023-11-10 18:16:32 -0500

    i

--- scripts/robot_writing.py
+++ scripts/robot_writing.py
@@ -181,7 +181,7 @@ if __name__ == "__main__":
     home_joint_state = [5.0, -1.80, -0.80, -2.0, 1.57, 0.1]
     # home_pos = [0.0, 0.4, 0.3]
     # # home_rot = [np.pi, 0, 0]
-    P0_pose = [0.85, 0.0, 0.75] 
+    P0_pose = [0.75, 0.0, 0.75] 
     P0_rot = [0.297, -3.144, 0.172]
 
    
@@ -189,7 +189,7 @@ if __name__ == "__main__":
     # P2 = [0.75, 0.10, 0.10, 1.279,2.883,-0.084]
     # P3 = [0.75, 0.0, 0.45, 1.279,2.883,-0.084]
     demo = BottlePickPlace()
-    angle = demo.ik(P0_pose,P0_rot)
+    angle = demo.ik_kdl(P0_pose)
     demo.close_gripper()
     demo.send_arm_traj(home_joint_state)
     demo.send_arm_traj(angle)
