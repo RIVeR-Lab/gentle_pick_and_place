2b26e779f31272eec9d7ffc1335186cbd5763ee9
Collins2 <collinsokara027@yahoo.com>
2024-09-26 18:48:42 -0400

    3

--- scripts/pick_and_place.py
+++ scripts/pick_and_place.py
@@ -110,7 +110,7 @@ class BottlePickPlace(object):
         traj_point = JointTrajectoryPoint()
         traj_point.positions = q
         traj_point.velocities = [0.0] * self.num_joints
-        traj_point.time_from_start = rospy.Time(1.5)
+        traj_point.time_from_start = rospy.Time(2.5)
 
         traj.points = [traj_point]
         traj_goal.trajectory = traj
@@ -129,7 +129,7 @@ class BottlePickPlace(object):
         grasp_pos = [
             object.pose.position.x,
             object.pose.position.y,
-            object.pose.position.z + 0.15
+            object.pose.position.z + 0.18
         ]
 
         grasp_rot_y = np.array([
