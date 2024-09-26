78b115f5a3e173bf6da79489ad7feddba93e4d25
Collins2 <collinsokara027@yahoo.com>
2024-09-26 18:46:36 -0400

    2

--- scripts/pick_and_place.py
+++ scripts/pick_and_place.py
@@ -129,7 +129,7 @@ class BottlePickPlace(object):
         grasp_pos = [
             object.pose.position.x,
             object.pose.position.y,
-            object.pose.position.z + 0.4
+            object.pose.position.z + 0.15
         ]
 
         grasp_rot_y = np.array([
